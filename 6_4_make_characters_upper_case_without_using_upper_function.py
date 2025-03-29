90bb5811cf4959a994bc0b52c574e73fc81c4de5
allenandreim <allenandrei8209@gmail.com>
2025-03-29 14:23:50 +0800

    code_output_6_4

--- 6_4_make_characters_upper_case_without_using_upper_function.py
+++ 6_4_make_characters_upper_case_without_using_upper_function.py
@@ -1,3 +1,17 @@
 # type something   
 # make characters upper without using upper() function
 # print output
+
+# type something   
+phrase = input("enter a string: ")
+result = ""
+
+# convert all lowercase characters to uppercase without using upper() function
+for char in phrase:
+    if 'a' <= char <= 'z':  
+        result += chr(ord(char) - 32)  
+    else:
+        result += char 
+
+# print the output
+print(result)
