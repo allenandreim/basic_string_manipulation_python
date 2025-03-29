c7a15b46f8a879a3040c4516661d246d2fd4417a
allenandreim <allenandrei8209@gmail.com>
2025-03-29 15:40:53 +0800

    code_output_6_5

--- 6_5_ljust_without_using_ljust_function.py
+++ 6_5_ljust_without_using_ljust_function.py
@@ -2,4 +2,21 @@
 # justifiy to left without using ljust()
 # print output
 
+# type something   
+# take user inputs for the string, width, and fill character
+phrase = input("enter a string: ")
+width = int(input("enter the desired width: "))
+fill_char = input("enter the fill character (default is space): ")
 
+# default space
+if not fill_char:
+    fill_char = ' '
+
+# calculate desired width
+padding = width - len(phrase)
+
+
+if padding > 0:
+    phrase += fill_char * padding
+
+print(f"'{phrase}'")
