b70c0d48b7210ca9931d9be2319782449d59805f
allenandreim <allenandrei8209@gmail.com>
2025-03-29 22:07:59 +0800

    code_output_6_7

--- 6_7_endswith_without_using_endswith_function.py
+++ 6_7_endswith_without_using_endswith_function.py
@@ -1,3 +1,17 @@
 # type something
 # check ending without using endswith() function
 # print output 
+
+# type something
+string = input("enter the main string: ")
+suffix = input("enter the ending to check: ")
+
+# check ending without using endswith() function
+if len(suffix) > len(string):
+    result = False
+else:
+    substring = string[-len(suffix):]
+    result = substring == suffix
+
+# print output 
+print( result)
