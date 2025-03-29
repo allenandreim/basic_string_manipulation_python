b8434a39eb48b8d61790b2b590a450b76f4cd853
allenandreim <allenandrei8209@gmail.com>
2025-03-29 13:55:23 +0800

    code_outout_5_4

---
+++ 5_4_make_name_all_lower.py
@@ -0,0 +1,12 @@
+# input full name
+# make it all lower 
+# print output
+
+# input full name
+name = input("full name: ")
+
+# make it all lower 
+lower_name = name.lower()
+
+# print output
+print(lower_name)
