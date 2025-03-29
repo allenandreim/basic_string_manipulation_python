a2ed468bce820ccb19861ace5c3a03f558c5d2ed
allenandreim <allenandrei8209@gmail.com>
2025-03-29 13:57:46 +0800

    code_output_5_6

---
+++ 5_6_make_name_reverse_case.py
@@ -0,0 +1,12 @@
+# input full name
+# make it reverse casing
+# print output
+
+# input full name
+name = input("full name: ")
+
+# make it reverse casing
+revcase_name = name.swapcase()
+
+# print output
+print(revcase_name)
