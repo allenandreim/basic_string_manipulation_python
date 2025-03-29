d6e38af5a81aeae3b7191d26c48649589e5cccaa
allenandreim <allenandrei8209@gmail.com>
2025-03-29 15:51:01 +0800

    code_output_6_6

--- 6_6_center_the_string_without_using_center_function.py
+++ 6_6_center_the_string_without_using_center_function.py
@@ -1,3 +1,19 @@
 # type something   
 # center without using center()
 # print output
+
+# type something   
+string = input("enter a string: ")
+width = int(input("enter the desired width: "))
+
+# center without using center()
+total_padding = width - len(string)
+
+# split padding evenly
+if total_padding > 0:
+    left_padding = total_padding // 2
+    right_padding = total_padding - left_padding
+    string = (' ' * left_padding) + string + (' ' * right_padding)
+
+# print output
+print(f"'{string}'")
