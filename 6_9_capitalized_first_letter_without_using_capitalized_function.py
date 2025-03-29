4026cd49e650cc88cb12ef2b6c78af555b7551a1
allenandreim <allenandrei8209@gmail.com>
2025-03-29 22:45:46 +0800

    code_output_6_9

--- 6_9_capitalized_first_letter_without_using_capitalized_function.py
+++ 6_9_capitalized_first_letter_without_using_capitalized_function.py
@@ -2,3 +2,14 @@
 # capitalized without using capitalized() function
 # print output 
 
+# type something
+phrase = input("enter a string: ")
+
+
+if len(phrase) > 0:
+    result = phrase[0].upper() + phrase[1:].lower()
+else:
+    result = phrase
+
+# print output
+print(result)
