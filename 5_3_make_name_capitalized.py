6ecbfce5e4ba9097777aae78b60b2a3a3a490db3
allenandreim <allenandrei8209@gmail.com>
2025-03-29 13:54:41 +0800

    code_output_5_3

---
+++ 5_3_make_name_capitalized.py
@@ -0,0 +1,12 @@
+# input full name
+# make it capitalized
+# print output
+
+# input full name
+name = input("full name: ")
+
+# make it capitalized
+caps_name = name.upper()
+
+# print output
+print(caps_name)
