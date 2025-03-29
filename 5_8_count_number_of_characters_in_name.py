0316bcd4f21708b4438c6a93d0f38dcd7d49606c
allenandreim <allenandrei8209@gmail.com>
2025-03-29 13:59:58 +0800

    code_output_5_8

---
+++ 5_8_count_number_of_characters_in_name.py
@@ -0,0 +1,12 @@
+# input full name
+# count number of characters
+# print output 
+
+# input full name
+name = input("full name: ")
+
+# count number of characters
+char_count = len(name)
+
+# print output 
+print(char_count)
