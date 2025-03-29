b4038c64ec2a3e84b79be163a2dcc9df76b7e1f4
allenandreim <allenandrei8209@gmail.com>
2025-03-29 13:56:58 +0800

    code_output_5_5

---
+++ 5_5_make_name_proper_casing.py
@@ -0,0 +1,12 @@
+# input full name
+# make it proper casing
+# print output
+
+# input full name
+name = input("full name: ")
+
+# make it proper casing
+procase_name = name.title()
+
+# print output
+print(procase_name)
