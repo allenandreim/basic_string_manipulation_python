c503ebc4d6e07471cb9ddbf5eb189a6103dd1043
allenandreim <allenandrei8209@gmail.com>
2025-03-29 23:14:11 +0800

    code_output_6_10

--- 6_10_title_case_without_using_title_function.py
+++ 6_10_title_case_without_using_title_function.py
@@ -1,3 +1,20 @@
 # type something
 # title case without using title() function
 # print output 
+
+# type something
+phrase = input("enter a string: ")
+words = phrase.split()
+result = ""
+
+# title case without using title() function
+for word in words:
+    if len(word) > 0:
+        capitalized_word = word[0].upper() + word[1:].lower()
+        result += capitalized_word + " "
+
+# title case without using title() function
+# print output 
+
+result = result.strip()
+print(result)
