12934064ff0fc40a4dc0eea0c9f6cc7b5b3e5e3f
allenandreim <allenandrei8209@gmail.com>
2025-04-01 20:14:49 +0800

    code_output_7_8

--- 7_8_count_characters_without_using_count_function.py
+++ 7_8_count_characters_without_using_count_function.py
@@ -1,3 +1,16 @@
 # type something
 # count specific characters without using count() function
 # print output 
+
+# type something
+phrase = input("enter a string: ")
+character = input("enter the character or substring to count: ")
+
+# count specific characters without using count() function
+count = 0
+for i in range(len(phrase) - len(character) + 1):
+    if phrase[i:i+len(character)] == character:
+        count += 1
+
+# print output
+print(count)
