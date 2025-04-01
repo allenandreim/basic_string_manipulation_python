6b18c67624ed501dd61b5161145b41b7ce16b2ad
allenandreim <allenandrei8209@gmail.com>
2025-04-01 20:33:18 +0800

    pseudocode_7_9

--- 7_9_find_location_of_characters_without_using_index_function.py
+++ 7_9_find_location_of_characters_without_using_index_function.py
@@ -1,3 +1,20 @@
 # type something
 # find location of characters without using index() function
 # print output 
+
+# type something
+phrase = input("enter a string: ")
+character = input("enter the substring to find: ")
+
+# find location of characters without using index() function
+position = -1  
+for i in range(len(phrase) - len(character) + 1):
+    if phrase[i:i + len(character)] == character:
+        position = i
+        break  
+
+# print output
+if position != -1:
+    print(position)
+else:
+    print(f"The substring '{character}' was not found in the string.")
