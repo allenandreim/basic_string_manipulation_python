644b18a0a77a5a75f2a4460bb19182139ce7983f
allenandreim <allenandrei8209@gmail.com>
2025-04-01 20:07:25 +0800

    code_output_7_77

--- 7_7_add_zeroes_without_using_zfill_function.py
+++ 7_7_add_zeroes_without_using_zfill_function.py
@@ -1,3 +1,15 @@
 # type something
 # add zeroes without using zfill() function
 # print output 
+
+# type something
+phrase = input("enter a string or number: ")
+width = int(input("enter the total width: "))
+
+# add zeroes without using zfill() function
+if len(phrase) < width:
+    zeros = width - len(phrase)
+    phrase = '0' * zeros + phrase
+
+# print output
+print(phrase)
