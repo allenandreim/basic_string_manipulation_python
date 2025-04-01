c7ef7d1dcc09743ebe546d53014107d984a57b83
allenandreim <allenandrei8209@gmail.com>
2025-04-01 16:47:28 +0800

    code_output_7_1

--- 7_1_rstrip_without_using_rstrip_function.py
+++ 7_1_rstrip_without_using_rstrip_function.py
@@ -2,3 +2,17 @@
 # remove characters you want like rstrip function but not using rstrip
 # print output
 
+# type something
+phrase = input("enter a string: ")
+
+# ask user for characters to remove
+chars_to_remove = input("enter characters to remove from the end: ")
+
+# remove chosen characters from the end of the string
+i = len(phrase) - 1
+while i >= 0 and phrase[i] in chars_to_remove:
+    i -= 1
+new_phrase = phrase[:i + 1]
+
+# Print output
+print(new_phrase)
