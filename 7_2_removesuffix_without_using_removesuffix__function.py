7c7fd4a2ee20c66e1c309985abf0bd764ed1b5b7
allenandreim <allenandrei8209@gmail.com>
2025-04-01 18:42:09 +0800

    code_output_7_2

--- 7_2_removesuffix_without_using_removesuffix__function.py
+++ 7_2_removesuffix_without_using_removesuffix__function.py
@@ -1,3 +1,14 @@
 # type something
 # removesuffix without using removesuffix() funtion
 # print output 
+
+# type something
+phrase = input("enter a string: ")
+suffix = input("enter the suffix to remove: ")
+
+# removesuffix without using removesuffix() funtion
+if phrase.endswith(suffix):
+    phrase = phrase[:-len(suffix)]
+
+# print output
+print(f"Processed string: '{phrase}'")
