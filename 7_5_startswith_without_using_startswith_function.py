e89505b10716daebe22bf5af2964d0b7afa9e8bb
allenandreim <allenandrei8209@gmail.com>
2025-04-01 19:29:19 +0800

    code_output_7_5

--- 7_5_startswith_without_using_startswith_function.py
+++ 7_5_startswith_without_using_startswith_function.py
@@ -2,3 +2,12 @@
 # startswith without usin startswith() function
 # print output 
 
+# Type something
+phrase = input("Enter a string: ")
+prefix = input("enter the prefix to check: ")
+
+# startswith without usin startswith() function
+starts_with_prefix = phrase[:len(prefix)] == prefix
+
+# print output
+print(starts_with_prefix)
