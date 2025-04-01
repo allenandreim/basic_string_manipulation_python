80ae3a82a4605ed59cab85bd0d47ef0014a65b4d
allenandreim <allenandrei8209@gmail.com>
2025-04-01 20:46:04 +0800

    code_output_7_10

--- 7_10_rindex_without_using_rindex_function.py
+++ 7_10_rindex_without_using_rindex_function.py
@@ -1,3 +1,20 @@
 # type something
 # find location of characters from start to last without using rindex() function
-# print outpu
+# print output
+
+# type something
+phrase = input("enter a string: ")
+target = input("enter the substring to find: ")
+
+# find location of characters from start to last without using rindex() function
+position = -1  
+for i in range(len(phrase) - len(target), -1, -1):  # Loop backward
+    if phrase[i:i + len(target)] == target:
+        position = i
+        break  
+
+# print output
+if position != -1:
+    print(f"'{target}' first appears at index {position} (starting from the end).")
+else:
+    print(f"The substring '{target}' was not found in the string.")
