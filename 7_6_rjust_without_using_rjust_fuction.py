64526f43ef66f97bff5e7eec94ff9326ec49a7d5
allenandreim <allenandrei8209@gmail.com>
2025-04-01 19:42:32 +0800

    code_output_7_6

--- 7_6_rjust_without_using_rjust_fuction.py
+++ 7_6_rjust_without_using_rjust_fuction.py
@@ -2,3 +2,14 @@
 # rjust without using rjust() function
 # print output 
 
+# type something
+phrase = input("enter a string: ")
+width = int(input("enter the total width: "))
+
+# rjust without using rjust() function
+if len(phrase) < width:
+    spaces = width - len(phrase)
+    phrase = ' ' * spaces + phrase
+
+# print output
+print(phrase)
