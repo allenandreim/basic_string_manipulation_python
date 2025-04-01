829e4d3323b9ff87b74838e654a034cdebd0660d
allenandreim <allenandrei8209@gmail.com>
2025-04-01 19:05:28 +0800

    code_output_7_4

--- 7_4_check_if_string_is_lower_without_using_islower_function.py
+++ 7_4_check_if_string_is_lower_without_using_islower_function.py
@@ -1,3 +1,16 @@
 # type something
 # check if string is lower without using islower() function
 # print output 
+
+# type something
+phrase = input("enter a string: ")
+all_lower = True  
+
+# check if string is lower without using islower() function
+for char in phrase:
+    if 'A' <= char <= 'Z':
+        all_lower = False
+        break 
+
+# print output
+print(all_lower)
