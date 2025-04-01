4f8f1de1531e2bcd03beebce6752b3f616d5c3ee
allenandreim <allenandrei8209@gmail.com>
2025-04-01 18:53:24 +0800

    code_output_7_3

--- 7_3_convert_string_to_upper_case_without_using_upper.py
+++ 7_3_convert_string_to_upper_case_without_using_upper.py
@@ -2,3 +2,16 @@
 # make upper case without using upper() function
 # print output
 
+# type something
+phrase = input("enter a string: ")
+
+# make upper case without using upper() function
+uppercase_phrase = ""
+for char in phrase:
+    if 'a' <= char <= 'z':  
+        uppercase_phrase += chr(ord(char) - 32)  
+    else:
+        uppercase_phrase += char 
+
+# print output
+print(uppercase_phrase)
